80506bf6b06ee56fd9c275ceba67e09e8e4e8533
Mihai Man <mihai.man@rover.com>
2023-01-13 19:22:00 +0200

    Update BerlinaleTicket script

--- berlinale/BerlinaleTicket.user.js
+++ berlinale/BerlinaleTicket.user.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         Berlinale ticket
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @downloadURL	 https://github.com/mitzaM/berlinale/raw/main/BerlinaleTicket.user.js
-// @description  try to take over the world!
 // @author       mitzaM
+// @description  try to take over the world!
+// @version      0.1
+// @namespace    https://mitzaman.ro/
+// @downloadURL	 https://github.com/mitzaM/user-scripts/raw/main/berlinale/BerlinaleTicket.user.js
+// @updateURL    https://github.com/mitzaM/user-scripts/raw/main/berlinale/BerlinaleTicket.meta.js
 // @match        https://www.eventim.de/*
 // @run-at       document-idle
 // @grant        none
 // ==/UserScript==
+
+const NR_OF_TICKETS = 1;
 
 (function() {
     'use strict';
@@ -16,7 +19,7 @@
     window.addEventListener('load', function() {
         var inputs = document.querySelectorAll('input[type=hidden].js-cc-amount-selection');
         inputs.forEach(function(input) {
-            input.value = 1;
+            input.value = NR_OF_TICKETS;
         });
         var add_cart = document.querySelectorAll('button[data-qa=TimeslotAddToShoppingCart1],button[data-qa=SeatAddToShoppingCart1],button[data-qa=AddToShoppingCart]')[0];
         if(add_cart) {
@@ -26,3 +29,4 @@
         }
     }, false);
 })();
+
