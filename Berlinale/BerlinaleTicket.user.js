a36e10c0e047e64a5d49150be01bed392746c82d
Mihai Man <mihai.man@rover.com>
2023-01-14 23:56:09 +0200

    Refactor Berlinale script

--- Berlinale/BerlinaleTicket.user.js
+++ Berlinale/BerlinaleTicket.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         Berlinale ticket
-// @author       mitzaM
-// @description  try to take over the world!
-// @version      0.1
-// @namespace    https://mitzaman.ro/
-// @downloadURL	 https://github.com/mitzaM/user-scripts/raw/main/berlinale/BerlinaleTicket.user.js
-// @updateURL    https://github.com/mitzaM/user-scripts/raw/main/berlinale/BerlinaleTicket.meta.js
-// @match        https://www.eventim.de/*
-// @run-at       document-idle
-// @grant        none
+// @name           Berlinale Ticket
+// @author         mitzaM
+// @description    try to take over the world!
+// @version        1.0.0
+// @namespace      https://mitzaman.ro/
+// @downloadURL    https://raw.githubusercontent.com/mitzaM/user-scripts/main/Berlinale/BerlinaleTicket.user.js
+// @updateURL      https://raw.githubusercontent.com/mitzaM/user-scripts/main/Berlinale/BerlinaleTicket.meta.js
+// @match          https://www.eventim.de/*
+// @run-at         document-idle
+// @grant          none
 // ==/UserScript==
 
 const NR_OF_TICKETS = 1;
@@ -22,11 +22,10 @@ const NR_OF_TICKETS = 1;
             input.value = NR_OF_TICKETS;
         });
         var add_cart = document.querySelectorAll('button[data-qa=TimeslotAddToShoppingCart1],button[data-qa=SeatAddToShoppingCart1],button[data-qa=AddToShoppingCart]')[0];
-        if(add_cart) {
+        if (add_cart) {
             add_cart.disabled = false;
             add_cart.classList.remove('disabled');
             add_cart.click();
         }
     }, false);
 })();
-
